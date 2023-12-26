652030202b7f6b37c7f8119db75ab177cebeee28
Marjan Shofy <76812554+mjshofy@users.noreply.github.com>
2023-12-26 10:24:17 +0600

    project files by mjshofy

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// selecting the html elements
+
+// activating the hamburger and mobile nav
+
+// hide mobile navigation after clicking a link or clicking outside
+
+// adding sticky animation to the navigation
+
+// mixitup for filtering the portfolio works
